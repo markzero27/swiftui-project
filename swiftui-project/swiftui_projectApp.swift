0c44d0870556a05f837f818258c7a5412e785996
Mark Daquis <36392970+markzero27@users.noreply.github.com>
2023-03-26 20:12:14 +0800

    Initial Commit

---
+++ swiftui-project/swiftui_projectApp.swift
@@ -0,0 +1,20 @@
+//
+//  swiftui_projectApp.swift
+//  swiftui-project
+//
+//  Created by Mark Daquis on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_projectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
